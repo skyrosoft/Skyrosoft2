85efe97fb4d769abd9c350457faf85a8d716ae1d
skyrosoft <125123066+skyrosoft@users.noreply.github.com>
2023-05-04 03:35:22 +0530

    Add files via upload

--- skyro1.js
+++ skyro1.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     $('.slider').slick({
-        centerMode: true,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
+        centerMode: true,
         autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
+        autoplaySpeed: 5000,
+        arrows: true,
+        dots: true,
+        appendDots: $('.slider-dots-box'),
+        dotsClass: 'slider-dots',
+        
         pauseOnHover: true,
         initialSlide: 0,
         infinite: true,
@@ -14,14 +17,21 @@ $(document).ready(function(){
         cssEase:'ease',
         draggable: true,
         easing:'linear',
-        pauseOnHover: true,
+
         respondTo:'window',
         swipe:true,
         touchMove:true,
         variableWidth: true,
 
-        
-        responsive: [{
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
             breakpoint: 600,
             settings: {
                 slidesToShow: 1,
@@ -36,4 +46,22 @@ $(document).ready(function(){
         }]
         });
     }); 
+
+
+    // On before slide change
+    $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $('.slider-dots-box button').html('');
+    }).trigger('beforeChange');
+  
+  // On before slide change
+  $('.slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.slider-dots-box button').html('');
+       $('.slider-dots-box .slick-active button')
+           .html(`<svg class="progress-svg" width="40" height="40">
+          <g transform="translate(20,20)">
+        <circle class="circle-go" r="19" cx="0" cy="0"></circle>
+        <text class="circle-tx" x="0" y="4" alignment-baseline="middle" stroke-width="0" text-anchor="middle">${(currentSlide || 0) + 1}</text>
+          </g>
+      </svg>`);
+  }).trigger('afterChange');
 
